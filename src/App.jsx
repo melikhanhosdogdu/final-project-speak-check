b7651f5f78dd084a3c1b4146bee366050faea72e
Melikhan Hoşdoğdu <melikhan.hosdogdu@gmail.com>
2025-11-12 15:01:08 -0600

    auth complate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Sidebar from "./components/Sidebar.jsx";
 import FeedPage from "../src/features/feed/FeedPage.jsx";
 import Login from "./features/auth/Login.jsx";
 import SignUp from "./features/auth/SignUp.jsx";
@@ -8,16 +9,23 @@ import GameRoomPage from "./features/game-room/GameRoomPage.jsx";
 import ProfilePage from "./features/profile/ProfilePage.jsx";
 
 function App() {
+  const location = useLocation();
+  const hideSidebar =
+    location.pathname === "/login" || location.pathname === "/signup";
+
   return (
-    <Routes>
-      <Route path="/" element={<FeedPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/listening-quiz" element={<ListeningPage />} />
-      <Route path="/game-room" element={<GameRoomPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/*" element={<Login />} />
-    </Routes>
+    <>
+      {!hideSidebar && <Sidebar />}
+      <Routes>
+        <Route path="/" element={<FeedPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/listening-quiz" element={<ListeningPage />} />
+        <Route path="/game-room" element={<GameRoomPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/*" element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
